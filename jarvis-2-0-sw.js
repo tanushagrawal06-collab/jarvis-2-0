b4792d27ccfab4b58aed4144a5319b5f46a4e4f0
tanushagrawal06-collab <tanushagrawal06@gmail.com>
2026-08-10 12:10:26 +0530

    Add files via upload

---
+++ jarvis-2-0-sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'jarvis-2-0-v1';
+const ASSETS = ['./jarvis-2-0.html', './jarvis-2-0.webmanifest'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match('./jarvis-2-0.html'))));
+});
